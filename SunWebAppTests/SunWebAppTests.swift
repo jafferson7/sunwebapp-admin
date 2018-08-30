ec74b8e83708aba802175123f0a8ee5aec645318
umjaffer <ujaffer@umich.edu>
2018-08-30 11:19:06 -0500

    Initial Commit

---
+++ SunWebAppTests/SunWebAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SunWebAppTests.swift
+//  SunWebAppTests
+//
+//  Created by Usmaan Jaffer on 8/30/18.
+//  Copyright © 2018 Usmaan Jaffer. All rights reserved.
+//
+
+import XCTest
+@testable import SunWebApp
+
+class SunWebAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
